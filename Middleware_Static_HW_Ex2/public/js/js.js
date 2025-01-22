97b4bde47dc227abc905eb3288c603a4f785554f
todosya1 <elieyzer1@gmail.com>
2025-01-22 15:17:23 +0200

    Delete Middleware_Static_HW_Ex2 directory

--- Middleware_Static_HW_Ex2/public/js/js.js
+++
@@ -1,8 +0,0 @@
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const headings = document.querySelectorAll('h1'); 
-    headings.forEach(heading => {
-        heading.style.color = 'red';
-});
-});
